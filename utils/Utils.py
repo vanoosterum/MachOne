e155fe46812c2737fb6ce98878ee39626ca966f9
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-04-29 10:56:57 +0100

    second commit utils 2

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 class Parameters():
-    def__init__(self):
+    def __init__(self):
     self.w = webdriver.Chrome('C:\Drivers\chromedriver') # To start Chrome
-    self.rootUrl = "http://qlv5-fe-qa.azurewebsites.net/" # To navigate to the Aareon Application
-    self.w.implicitly_wait (20) # To allow user to see the progress. Can be commented out for behind the scene tests
+    self.rootUrl = ('http://qlv5-fe-qa.azurewebsites.net/') # To navigate to the Aareon Application
+    self.w.implicitly_wait(20)
