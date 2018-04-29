c4eac1c457e5e935e1185b47e3e4dccd55fecb33
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-04-29 10:49:35 +0100

    second commit utils

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,7 +1,6 @@
-import time
 from selenium import webdriver
-
-driver = webdriver.Chrome('C:\Drivers\chromedriver') # To start Chrome
-driver.get('http://qlv5-fe-qa.azurewebsites.net/') # To navigate to the Aareon Application
-
-time.sleep(20) # To allow user to see the progress. Can be commented out for behind the scene tests
+class Parameters():
+    def__init__(self):
+    self.w = webdriver.Chrome('C:\Drivers\chromedriver') # To start Chrome
+    self.rootUrl = "http://qlv5-fe-qa.azurewebsites.net/" # To navigate to the Aareon Application
+    self.w.implicitly_wait (20) # To allow user to see the progress. Can be commented out for behind the scene tests
