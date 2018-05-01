2d76ae6105d37e9222e8bbfa3bd8a6fd56e87ebf
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-01 14:21:26 +0100

    util initial

---
+++ utils/utils.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+
+class Parameters():
+    def __init__(self):
+        self.w = webdriver.Chrome('C:\Drivers\chromedriver') # To start Chrome
+        self.rootUrl = "http://qlv5-fe-qa.azurewebsites.net/" # To navigate to the Aareon Application
