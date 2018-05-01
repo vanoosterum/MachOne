e0d36e7db6263a67841e6620b32f3756e42f25f1
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-01 12:27:16 +0100

    foobar

--- utils/Utils.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-class Parameters():
-    def __init__(self):
-    self.w = webdriver.Chrome('C:\Drivers\chromedriver') # To start Chrome
-    self.rootUrl = ('http://qlv5-fe-qa.azurewebsites.net/') # To navigate to the Aareon Application
-    self.w.implicitly_wait(20)
