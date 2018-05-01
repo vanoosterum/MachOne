099929acc32b05c48785589547179485e5856bb5
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-01 17:00:04 +0100

    remove test from setUp

--- tests/base_test.py
+++ tests/base_test.py
@@ -7,5 +7,5 @@ class BaseTest(unittest.TestCase):
     param = Parameters()
     loginpage = LoginPage(param.w, param.rootUrl)
 
-    def test_setUp(self):
+    def setUp(self):
         self.param.w.get(self.param.rootUrl)
