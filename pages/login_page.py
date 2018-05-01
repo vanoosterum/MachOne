64c4a40be4342c2c48fad09af975e54dd2c6def3
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-01 14:23:53 +0100

    login reformat

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,5 +3,5 @@ from page_objects import PageObject, PageElement
 
 class LoginPage(PageObject):
 
-	def check_page(self):
-		return "QL V5 Authentication" in self.w.title
+    def check_page(self):
+        return "QL V5 Authentication" in self.w.title
