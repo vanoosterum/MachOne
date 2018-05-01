e8bc9f220998216702e1b0ee4ab2eeef88c03d0c
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-01 16:19:34 +0100

    page title correction

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,4 +4,4 @@ from page_objects import PageObject, PageElement
 class LoginPage(PageObject):
 
     def check_page(self):
-        return "QL V5 Authentication" in self.w.title
+        return "QL" in self.w.title
