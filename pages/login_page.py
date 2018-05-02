8c3b3ab00e1f814d59abd6d75a69ff2d9349e8de
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-02 14:58:10 +0100

    added login

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,14 @@ from page_objects import PageObject, PageElement
 
 class LoginPage(PageObject):
 
-
     def check_page(self):
-        return "QL V5 Authentication" in self.w.title
+        return "Authentication" in self.w.title
+
+username_field = PageElement(id_ = "username")
+password_field = PageElement(id_ = "password")
+
+def login(self, homepage, username, password):
+        self.username_field.send_keys(username)
+        self.password_field.send_keys(password)
+        self.password_field.submit()
+        return homepage.check_page()
