a428afa566c35984fc7f0c29b706332d65dcedbf
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-02 14:59:05 +0100

    added homepage

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,11 +1,13 @@
 import unittest
 from utils import Parameters
 from login_page import LoginPage
+from home_page import HomePage
 
 
 class BaseTest(unittest.TestCase):
     param = Parameters()
     loginpage = LoginPage(param.w, param.rootUrl)
+    homepage = HomePage(param.w, param.rootUrl)
 
-    def setUp(self):
+    def test_setUp(self):
         self.param.w.get(self.param.rootUrl)
