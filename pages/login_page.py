80f65f6a759f007ffff6e8e30062e9345e3c10fe
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-02 16:04:54 +0100

    version commit

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,11 +6,13 @@ class LoginPage(PageObject):
     def check_page(self):
         return "Authentication" in self.w.title
 
-username_field = PageElement(id_ = "username")
-password_field = PageElement(id_ = "password")
+
+username_field = PageElement(id_="username")
+password_field = PageElement(id_="password")
+
 
 def login(self, homepage, username, password):
-        self.username_field.send_keys(username)
-        self.password_field.send_keys(password)
-        self.password_field.submit()
-        return homepage.check_page()
+    self.username_field.send_keys(username)
+    self.password_field.send_keys(password)
+    self.password_field.submit()
+    return homepage.check_page()
