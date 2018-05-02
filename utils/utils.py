5e705dcd5d888470e6d0943241309f23c939d814
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-02 15:00:24 +0100

    commented out the implicit wait

--- utils/utils.py
+++ utils/utils.py
@@ -5,4 +5,4 @@ class Parameters():
     def __init__(self):
         self.w = webdriver.Chrome('C:\Drivers\chromedriver')  # To start Chrome
         self.rootUrl = "http://qlv5-fe-qa.azurewebsites.net/"  # To navigate to the Aareon Application
-        self.w.implicitly_wait(10)
+        # self.w.implicitly_wait(10)
