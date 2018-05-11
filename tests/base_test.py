8ad05ce57ac921c7679f14eaaf69a464d1e12ed2
vanoosterum <38810765+vanoosterum@users.noreply.github.com>
2018-05-11 15:32:26 +0100

    test amendments

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,15 +1,18 @@
 import unittest
-
-import utils
+from utils import Parameters
 from login_page import LoginPage
 from home_page import HomePage
 
 
-class BaseTest (unittest.TestCase):
-    param = utils.Parameters()
-    loginpage = LoginPage(param.w, param.rootUrl)
-    homepage = HomePage(param.w, param.rootUrl)
+class BaseTest(unittest.TestCase):
+    param = Parameters()
+    loginpage = LoginPage(param.w, param.url)
+    homepage = HomePage(param.w, param.url)
 
-    def setUp(self):
-        self.param.w.get(self.param.rootUrl)
-        #       assert self.loginpage.check_page()
+    def test_setUp(self):
+        self.param.w.visit(self.param.url)
+        assert self.loginpage.check_page()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.param.w.quit()
